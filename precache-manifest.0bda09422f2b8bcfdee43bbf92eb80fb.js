1cd85291eaf5867ba2f1dda3414cfb34a94d0b80
Chris Cosentino <chriscosentino@hotmail.com>
2020-10-14 03:17:29 -0400

    Updates

---
+++ precache-manifest.0bda09422f2b8bcfdee43bbf92eb80fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c4ccaae38958e85742ff0021600df3",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "1171d0db897b4a09e26b",
+    "url": "/pokemon/static/css/main.d9933206.chunk.css"
+  },
+  {
+    "revision": "2757a22be0ce59bb3414",
+    "url": "/pokemon/static/js/2.8c044adb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon/static/js/2.8c044adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1171d0db897b4a09e26b",
+    "url": "/pokemon/static/js/main.30cbc065.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  }
+]);
